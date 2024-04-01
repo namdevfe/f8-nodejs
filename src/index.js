c586be195c02a495a2358b0c79db1d7344e1f3ba
nguyenkimquocnam-dev <nguyenkimquocnam.dev@gmail.com>
2024-04-01 14:08:38 +0700

    refactor: add routes & controllers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const hbs = require("express-handlebars");
 const path = require("path");
+const route = require("./routes");
 const app = express();
 const port = 3000;
 
@@ -9,7 +10,11 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 // Apply middleware
-app.use(express.urlencoded());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 // HTTP logger
@@ -20,22 +25,8 @@ app.engine(".hbs", hbs.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log("🚀req.body---->", req.body);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
